c21127da79e4647092ba5d4b2ec91f7b87278e8a
Abdullah <mohdabdullah.it@gmail.com>
2021-05-23 14:48:25 +0530

    modules completed

---
+++ breaking-bad-cast/src/components/ui/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../../img/logo.png'
+
+const Header = (props) => {
+  return (
+    <header className='center'>
+      <img src={logo} alt='' />
+    </header>
+  )
+}
+
+export default Header
